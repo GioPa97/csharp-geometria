83236eb35de3281f857b418ca02a43db8c7a60c4
GioPa97 <giovannipagliara97@.com>
2022-11-25 16:52:01 +0100

    Funzione stampa da correggere

--- csharp-geometria/Class1.cs
+++ csharp-geometria/Class1.cs
@@ -7,6 +7,8 @@
         public int baseRettangolo = 20;
         public int altezzaRettangolo = 10;
         public string nomeRettangolo;
+        public int perimetro;
+        public int area;
 
 
 
@@ -24,6 +26,7 @@
         {
             int perimetro = (baseRettangolo + altezzaRettangolo) * 2;
             return perimetro;
+
         }
 
         public void stampaRettangolo()
@@ -31,6 +34,11 @@
             Console.WriteLine($"---Informazioni Rettangolo {this.nomeRettangolo}---");
             Console.WriteLine($"Altezza: {this.altezzaRettangolo}");
             Console.WriteLine($"Base: {this.baseRettangolo}");
+            calcolaPerimetro(this.baseRettangolo, this.altezzaRettangolo);
+            calcolaArea(this.baseRettangolo, this.altezzaRettangolo);
+            Console.WriteLine(this.perimetro);
+            Console.WriteLine(this.area);
+            Console.WriteLine("---------------------------------------------------");
         }
 
 
